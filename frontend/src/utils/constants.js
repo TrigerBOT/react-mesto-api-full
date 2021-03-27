f73abb967c336f3da175051ca98a2d40280fef79
TrigerBOT <night3596248@gmail.com>
2021-03-27 15:22:59 +0300

    server fix 2

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,6 @@
-export const baseUrl = 'https://mesto.nomoreparties.co/v1/cohort-16';
-export const authorization = '0100295d-ffab-4dd9-a2ae-64af071cc3da';
+export const baseUrl = 'http://api.kirill-trigerbot.nomoredomains.icu/';
+export const authorization = localStorage.getItem('jwt');
 
 export const BASE_URL = 'https://auth.nomoreparties.co';
+
+
