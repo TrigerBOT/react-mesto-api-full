2b7a83393e388f894e439e770b1b08eb5f5cf5c2
TrigerBOT <night3596248@gmail.com>
2021-03-29 19:35:55 +0300

    dsad

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const baseUrl = `${window.location.protocol}${'//localhost:3001'}`
+export const baseUrl = `${window.location.protocol}${process.env.REACT_APP_API_URL ||  '//localhost:3001'}`
 export const authorization = `${localStorage.getItem('JWT')}`;
 
 
